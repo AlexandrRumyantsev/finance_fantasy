fd93a9daa859df8101fcd9275baccac1966566f3
al.rumyantsev <al.rumyantsev@proton-group.ru>
2025-06-20 19:08:18 +0300

    mock

--- lib/presentation/summary/logic/incomes/cubit.dart
+++ lib/presentation/summary/logic/incomes/cubit.dart
@@ -1,3 +1,7 @@
+import 'package:finance_fantasy/domain/entities/category.dart';
+import 'package:finance_fantasy/domain/entities/transaction_extended.dart';
+
+import '../../../../domain/entities/account_brief.dart';
 import '../../../../domain/entities/status_page.dart';
 import '../cubit.dart';
 
@@ -5,10 +9,16 @@ class IncomesSummaryCubit extends BaseSummaryCubit {
   @override
   Future<void> loadData() async {
     await Future.delayed(const Duration(seconds: 3));
-    if (state.transactions?.isNotEmpty ?? false) {
-      emit(state.copyWith(statusPage: StatusPage.data));
-    } else {
-      emit(state.copyWith(statusPage: StatusPage.empty));
-    }
+    emit(state.copyWith(statusPage: StatusPage.data, transactions: [
+      TransactionExtended(
+        id: 0,
+        amount: 1000000,
+        transactionDate: DateTime.now(),
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+        account: AccountBrief(name: 'Cash', currency: 'USD', balance: 123214124, id: 0),
+        category: Category(id: 0, name: 'Дом', emoji: '🏠', isIncome: true),
+      )
+    ]));
   }
 }
